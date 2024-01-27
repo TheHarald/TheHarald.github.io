3200a5b6ebdd904083949d972e259a17476c05bc
Roman Surinov <romasurinov13@mail.ru>
2024-01-28 01:27:44 +0300

    try fix actions

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'dummyimage.com',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    },
 };
 
 module.exports = nextConfig
